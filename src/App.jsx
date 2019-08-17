11b4366c3492989de04eb006621ec7c43cd81056
ramazantufekci <ramazan.tufekci@outlook.com>
2019-08-17 08:51:45 +0300

    bind addStudent to App

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
     this.handleButtonClicked = this.handleButtonClicked.bind(this);
     this.addAssignment = this.addAssignment.bind(this);
     /*Uncomment the line below to bind the method*/
-    /*this.addStudent = this.addStudent.bind(this);*/
+    this.addStudent = this.addStudent.bind(this);
     this.addGrade = this.addGrade.bind(this);
   }
 
@@ -36,7 +36,7 @@ class App extends React.Component {
   /*Write an addStudent method here*/
   addStudent(studentName)
   {
-	  let studentName=students;
+	  
   }
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
